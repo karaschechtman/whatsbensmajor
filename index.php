bcb4880ac43646fc118e2bb1fcee35eae8b01c34
Kara Schechtman <kws2121@columbia.edu>
2017-01-28 19:29:16 -0500

    index'

--- index.php
+++ index.php
@@ -10,22 +10,18 @@
     </head>
 	<body>
             <?php
-            # This function reads your DATABASE_URL config var and returns a connection
-            # string suitable for pg_connect. Put this in your app.
             function pg_connection_string_from_database_url() {
                 extract(parse_url($_ENV["DATABASE_URL"]));
                 return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there                too
             }
 
-            echo " <div class = \"cornericons\">
-            <span class=\"glyphicon glyphicon-envelope\"></span>
-            <span class=\"glyphicon glyphicon-pencil\"></span>
-            <span class=\"glyphicon glyphicon-question-sign\"></span>
-        </div>
+            echo "<div class = \"cornericons\">
+                <span class=\"glyphicon glyphicon-envelope\"></span>
+                <span class=\"glyphicon glyphicon-pencil\"></span>
+                <span class=\"glyphicon glyphicon-question-sign\"></span>
+            </div>
             ";
-            # Here we establish the connection. Yes, that's all.
             $pg_conn = pg_connect(pg_connection_string_from_database_url());
-            # Now let's use the connection for something silly just to prove it works:
             $result = pg_query($pg_conn, "SELECT * FROM benmajor");
             if (!pg_num_rows($result)) {
                 echo 'The database is empty!';
